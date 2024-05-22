53533eea046b79b0887c7a02885b161e23bd7cd8
rscres <renatocrescenti@gmail.com>
2024-05-21 23:56:07 -0300

    first commit

---
+++ anatomia/src/edu/renato/firstweek/MinhaClasse.java
@@ -0,0 +1,11 @@
+public class MinhaClasse {
+    
+    String nome = "Renato";
+    int idade = 31;
+
+    public static void main(String[] args) {
+        MinhaClasse mc = new MinhaClasse();
+        System.out.println(mc.nome);
+    }
+
+}
